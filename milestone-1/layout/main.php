1ec75f89500900912fb0c0657ad35447fb0539e8
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-05-24 16:29:46 +0200

    Mil 1: Add main section

--- milestone-1/layout/main.php
+++ milestone-1/layout/main.php
@@ -1,16 +1,11 @@
 
+    <main class="d-flex align-items-center justify-content-center">
+        
+        <?php 
+            include __DIR__ . '/../partials/albums.php'; 
+        ?>
 
-
-
-
-
-
-
-
-
-
-
-
+    </main>
 
 </body>
 </html>
